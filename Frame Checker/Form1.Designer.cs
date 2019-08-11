ad832df1c39c3c7864ff157024be89403f448c12
ahsanGoheer <36065735+ahsanGoheer@users.noreply.github.com>
2019-08-11 12:56:28 +0500

    Add project files.

---
+++ Frame Checker/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Frame_Checker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(39, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(264, 233);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(800, 46);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(265, 233);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(79, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 38);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Input Frame";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Script", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(784, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(294, 38);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Matched Video Frame";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(510, 334);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(251, 30);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(367, 313);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 78);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Add Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(367, 445);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 78);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Add Video";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(510, 466);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(251, 30);
+            this.textBox2.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(510, 719);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(151, 78);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Check Frames";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BackgroundWorker1_ProgressChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.Crimson;
+            this.progressBar1.Location = new System.Drawing.Point(744, 734);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(334, 29);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.ClientSize = new System.Drawing.Size(1122, 809);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Frame Checker ";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Frame Checker/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace Frame_Checker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            backgroundWorker1.DoWork += BackgroundWorker1_DoWork;
+            backgroundWorker1.ProgressChanged += BackgroundWorker1_ProgressChanged;
+            backgroundWorker1.WorkerReportsProgress = true;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog()==DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog video_di = new OpenFileDialog();
+            video_di.Filter = "Video Files(*.MP4)|*.MP4";
+
+            if (video_di.ShowDialog()==DialogResult.OK)
+            {
+                textBox2.Text = video_di.FileName;
+
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void BackgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            List<string> files = new List<string>();
+            files = Directory.GetFiles(textBox1.Text,"*.jpg").ToList<string>();
+            //progressBar1.Minimum = 0;
+            //progressBar1.Maximum = files.Count;
+            //Set_ProgressBar(0,files.Count);
+            
+
+            foreach (string file in files)
+            {
+                pictureBox1.Image = null;
+                pictureBox1.ImageLocation = file;
+                Emgu.CV.VideoCapture videoCapture = new VideoCapture(textBox2.Text);
+                Mat image = new Mat();
+                Mat Img2 = new Mat();
+                Mat img = new Mat(file);
+                Emgu.CV.CvInvoke.Resize(img, img, new Size(16, 16), 0, 0, Emgu.CV.CvEnum.Inter.Linear);
+                double nrmse = 0;
+                Point point = new Point();
+                point.X = 510;
+                point.Y = 566;
+                TextBox newtext_Box = new TextBox();
+                newtext_Box.Location = point;
+                newtext_Box.Visible = true;
+                while (videoCapture.IsOpened)
+                {
+                    image = videoCapture.QueryFrame();
+                    Emgu.CV.CvInvoke.Resize(image, Img2, new Size(16, 16), 0, 0, Emgu.CV.CvEnum.Inter.Linear);
+                    nrmse = calc_mse(img, Img2);
+                    newtext_Box.Text = nrmse.ToString();
+                   
+                    //textBox2.Text = calc_mse(img, image).ToString();
+                    if (nrmse <= 0.05)
+                    {
+                        pictureBox2.Image = image.Bitmap;
+                        break;
+
+                    }
+                    else if(nrmse<=0.1)
+                    {
+                        pictureBox2.Image = image.Bitmap;
+                        break;
+                    }
+                    else if(nrmse<=0.15)
+                    {
+                        pictureBox2.Image = image.Bitmap;
+                        break;
+                    }
+                   
+
+                }
+                System.Threading.Thread.Sleep(1000);
+                backgroundWorker1.ReportProgress(1);
+                //System.Threading.Thread.Sleep(1000);
+            }
+
+        }
+
+        private void BackgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+
+        }
+        void Set_ProgressBar(int start,int end)
+        {
+            progressBar1.Maximum = start;
+            progressBar1.Minimum = end;
+        }
+
+        //public byte[] imageToByteArray(System.Drawing.Image imageIn)
+        //{
+        //    MemoryStream ms = new MemoryStream();
+        //    imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+        //    return ms.ToArray();
+        //}
+        public double calc_mse(Mat image1, Mat image2)
+        {
+            double diff;
+            byte img1;
+            byte img2;
+            //float nrmse = 0;
+            double sum = 0;
+            double total_size = image1.Width * image1.Height;
+            //float y1=0, y2=0;
+            double max=0, min=0;
+            for (int x = 0; x < image1.Width; x++)
+            {
+                for (int y = 0; y < image1.Height; y++)
+                {
+                   img1= image1.ToImage<Gray,Byte>().Data[x,y,0];
+                   img2 = image2.ToImage<Gray, Byte>().Data[x, y, 0];
+                   diff = img1 - img2;
+                    if(max==0)
+                    {
+                        max = diff;
+                    }
+                   if(max<diff)
+                    {
+                        max = diff;
+                    }
+                   if(diff<min)
+                    {
+                        min = diff;
+                    }
+                   sum += (diff * diff);
+                }
+            }
+            return ((System.Math.Sqrt(sum / total_size))/(max-min));
+        }
+    }
+
+}
